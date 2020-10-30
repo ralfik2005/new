f3208ee1ae54a08bb5b28c5702c4ac6a8e979681
sptest234 <stayprivatetester@gmail.com>
2020-10-30 17:18:16 +0000

    friday update two

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const login = 'admin';
+const password = '123';
 
 /* GET home page. */
 router.get('/', (req, res ) => {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', (req, res ) => {
+  res.render('login', { title: 'Login' });
+});
+
+router.post('/login', (req, res) => {
+ // console.log(req.body);
+  const body = req.body;
+
+  if(body.login === login && body.password === password) {
+    res.redirect('/admin');
+  } else {
+    res.redirect('/login');
+  }
+  
+});
+
 
 //hello
 module.exports = router;
