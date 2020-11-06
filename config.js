672523ca5dd96738d6313a5a3e26c8694d07e382
sptest234 <stayprivatetester@gmail.com>
2020-11-06 11:06:19 +0000

    admion login

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:rYiupu3Nn14UV5es@cluster0.qrbew.mongodb.net/<dbname>?retryWrites=true&w=majority',
     keySession: ['KACZORKEY'],
     MAXaGEsESSION: 24 * 60 * 60 * 1000
 }
